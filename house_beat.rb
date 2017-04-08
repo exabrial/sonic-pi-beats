052cb05b144f0383f4006a8666170b25b7a3fe6d
Jonathan S. Fisher <jfisher@tomitribe.com>
2017-04-07 19:36:49 -0500

    level some of the samples

--- house_beat.rb
+++ house_beat.rb
@@ -1,62 +1,74 @@
-use_bpm 122
+use_bpm 124
 
 live_loop :bass_drum do
-  # mark the 4 count of the song with emphasis on 1 and 4
-  # beat 1
-  sample :drum_heavy_kick
-  sample :bd_klub
-  sleep (1)
-  
-  # beat 2
-  sample :drum_heavy_kick
-  sleep (1)
-  
-  # beat 3
-  sample :drum_heavy_kick
-  sleep (1)
-  
-  # beat 4
-  sample :drum_heavy_kick
-  sample :bd_klub
-  sleep (1)
-end
-
-live_loop :snare do
-  # snare hits on beat 2 and 4
-  with_fx :reverb do
+  with_fx :level, amp: 0.85  do
+    # mark the 4 count of the song with emphasis on 1 and 4
+    # beat 1
+    sample :drum_heavy_kick
+    sample :bd_klub
     sleep (1)
-    sample :drum_snare_soft
+    
+    # beat 2
+    sample :drum_heavy_kick
     sleep (1)
+    
+    # beat 3
+    sample :drum_heavy_kick
     sleep (1)
-    sample :drum_snare_soft
+    
+    # beat 4
+    sample :drum_heavy_kick
+    sample :bd_klub
     sleep (1)
   end
 end
 
+live_loop :snare do
+  # snare hits on beat 2 and 4
+  with_fx :level, amp: 0.5  do
+    with_fx :reverb, room: 1 do
+      sleep (1)
+      sample :drum_snare_soft
+      sleep (1)
+      sleep (1)
+      sample :drum_snare_soft
+      sleep (1)
+    end
+  end
+end
+
+high_hat_level = 0.3
+
 live_loop :offset_hats do
-  # these fall on the offbeats but count the beat of the song
-  sleep 0.5
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 0.5
+  with_fx :level, amp: high_hat_level do
+    # these fall on the offbeats but count the beat of the song
+    sleep 0.5
+    sample :drum_cymbal_closed
+    sleep 1
+    sample :drum_cymbal_closed
+    sleep 1
+    sample :drum_cymbal_closed
+    sleep 1
+    sample :drum_cymbal_closed
+    sleep 0.5
+  end
 end
 
 live_loop :moving_high_hat do
-  # add offset highhat that moves cyclicaly around the beat every 4 measures
-  sleep 3.25
-  sample :drum_cymbal_closed
+  with_fx :level, amp: high_hat_level do
+    # add offset highhat that moves cyclicaly around the beat with a period of 12 measures
+    sleep 3.25
+    sample :drum_cymbal_closed
+  end
 end
 
 live_loop :emphasis_hat do
-  # add extra high hat that falls regularly
-  sleep 3.75
-  sample :drum_cymbal_closed
-  sleep 1.25
+  with_fx :level, amp: high_hat_level do
+    # add extra high hat that falls regularly
+    sleep 3.75
+    sample :drum_cymbal_closed
+    sleep 1.25
+  end
 end
 
 live_loop :mark8 do
@@ -87,24 +99,20 @@ define :chord_player do |root, repeats|
 end
 
 define :chord_thing do
-  with_fx :reverb do
-    with_fx :reverb do
-      with_fx :reverb do
-        # total beats: 12
-        # you can move the timings around
-        # as long as the count doesn't exceed 12
-        chord_player :e3, 4
-        sleep 0.5
-        chord_player :a3, 1
-        sleep 2
-        chord_player :d3, 2
-        sleep 1
-        chord_player :d3, 1
-        sleep 0.5
-        chord_player :e3, 5
-        sleep 1.5
-      end
-    end
+  with_fx :reverb, room:1 do
+    # total beats: 12
+    # you can move the timings around
+    # as long as the count doesn't exceed 12
+    chord_player :e3, 4
+    sleep 0.5
+    chord_player :a3, 1
+    sleep 2
+    chord_player :d3, 2
+    sleep 1
+    chord_player :d3, 1
+    sleep 0.5
+    chord_player :e3, 5
+    sleep 1.5
   end
 end
 
