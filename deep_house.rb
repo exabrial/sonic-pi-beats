5c962ba4aafd67af91f150fe4cb06f992417bd03
Jonathan S. Fisher <jfisher@tomitribe.com>
2017-05-12 20:05:36 -0500

    tuning n stuff

--- deep_house.rb
+++ deep_house.rb
@@ -2,7 +2,6 @@ use_bpm 124
 high_hat_level = 0.3
 extra_samples="/Users/jonathanfisher/dev/sonic-pi-beats/extra_samples"
 
-
 live_loop :mark128 do
   sync :bass_drum
   with_fx :level, amp: 1.25 do
@@ -13,7 +12,7 @@ live_loop :mark128 do
 end
 
 
-kick_enabled = true
+kick_enabled=false
 
 live_loop :bass_drum do
   with_fx :level, amp: 0.85  do
@@ -21,7 +20,7 @@ live_loop :bass_drum do
     # beat 1
     if kick_enabled then
       sample :drum_heavy_kick
-      sample :bd_klub, amp: 0.75
+      sample :bd_klub, amp: 0.65
     end
     sleep (1)
     
@@ -43,7 +42,7 @@ live_loop :bass_drum do
     # beat 4
     if kick_enabled then
       sample :drum_heavy_kick
-      sample :bd_klub, amp: 0.75
+      sample :bd_klub, amp: 0.65
     end
     with_fx :reverb, room: 0.85 do
       sample extra_samples, :hand_clap, amp: 1.25
@@ -52,23 +51,31 @@ live_loop :bass_drum do
   end
 end
 
+hats_enabled=false
+
 live_loop :hats_and_snares do
-  sleep 0.5
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 1
-  sample :drum_cymbal_closed
-  sleep 0.5
+  sync :bass_drum
+  
+  if (hats_enabled) then
+    sleep 0.5
+    sample :drum_cymbal_closed, amp: high_hat_level
+    sleep 1
+    sample :drum_cymbal_closed, amp: high_hat_level
+    sleep 1
+    sample :drum_cymbal_closed, amp: high_hat_level
+    sleep 1
+    sample :drum_cymbal_closed, amp: high_hat_level
+    sleep 0.5
+  end
 end
+
 
 live_loop :emphasis_hat do
   sync :bass_drum
-  with_fx :level, amp: high_hat_level do
+  
+  if (hats_enabled) then
     sleep 3.75
-    sample :drum_cymbal_closed
+    sample :drum_cymbal_closed, amp: high_hat_level
     sleep 0.25
   end
 end
@@ -81,12 +88,12 @@ define :chord_player do |root, repeats|
   end
 end
 
-bass_enabled=true
-e_enabled=true
+bass_enabled=false
+e_enabled=false
 
 live_loop :chord do
   sync :bass_drum
-
+  
   sleep 0.5
   if e_enabled then
     play chord(:e3, :minor), release: 0.3
@@ -94,7 +101,7 @@ live_loop :chord do
   if bass_enabled then
     play note(:e2), release: 0.5, amp: 1.5
   end
-
+  
   sleep 1
   if e_enabled then
     play chord(:e3, :minor), release: 0.3
@@ -102,7 +109,7 @@ live_loop :chord do
   if bass_enabled then
     play note(:e2), release: 0.5, amp: 1.5
   end
-
+  
   sleep 1
   if e_enabled then
     play chord(:e3, :minor), release: 0.3
@@ -110,7 +117,7 @@ live_loop :chord do
   if bass_enabled then
     play note(:e2), release: 0.5, amp: 1.5
   end
-
+  
   sleep 1.5
   if e_enabled then
     play chord(:e3, :minor), release: 0.3
@@ -118,7 +125,7 @@ live_loop :chord do
   if bass_enabled then
     play note(:e2), release: 0.5, amp: 1.5
   end
-
+  
   sleep 0.5
   if e_enabled then
     play chord(:e3, :minor), release: 0.3
